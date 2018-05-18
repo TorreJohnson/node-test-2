4ab49c531ae6290894ff8620571644e07dbec5c0
TorreJohnson <torre.johnson@gmail.com>
2018-05-17 22:31:06 -0400

    added event listeners to usernames

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,7 +1,21 @@
 // Userlist data array for filling in info box
 var userListData = [];
 
-populateTable();
+document.addEventListener("DOMContentLoaded", e => {
+	console.log("DOM fully loaded");
+	populateTable();
+});
+
+// Username link click
+function addEventListenersToUsernames() {
+	var links = document.getElementsByClassName("linkshowuser");
+	for (var i = 0; i < links.length; i++) {
+		links[i].addEventListener("click", e => {
+			e.preventDefault();
+			showUserInfo(e);
+		});
+	}
+}
 
 // FUNCTIONS
 
@@ -22,7 +36,9 @@ function populateTable() {
 	xmlhttp.open("GET", url, true);
 	xmlhttp.send();
 
+	// For each item in our JSON, add a table row and cells to the content string
 	function formatResponseForTable(res) {
+		userListData = res;
 		for (var i = 0; i < res.length; i++) {
 			tableContent += "<tr>";
 			tableContent +=
@@ -38,7 +54,28 @@ function populateTable() {
 				'">delete</a></td>';
 			tableContent += "</tr>";
 		}
+		// Inject the whole content string into our existing HTML table
 		var table = document.getElementsByTagName("tbody")[0];
 		table.innerHTML += tableContent;
+		addEventListenersToUsernames();
 	}
+}
+
+// Show User Info
+function showUserInfo(e) {
+	// Retrieve username from link rel attribute
+	var thisUserName = e.target.rel;
+	// Get index of object based on id value
+	var arrayPosition = userListData
+		.map(arrayItem => arrayItem.username)
+		.indexOf(thisUserName);
+	// Get our User Object
+	var thisUserObject = userListData[arrayPosition];
+	// Populate Info Box
+	document.getElementById("userInfoName").innerText(thisUserObject.fullname);
+	document.getElementById("userInfoAge").innerText(thisUserObject.age);
+	document.getElementById("userInfoGender").innerText(thisUserObject.gender);
+	document
+		.getElementById("userInfoLocation")
+		.innerText(thisUserObject.location);
 }
